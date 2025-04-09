d858095c9ea41eaf5ac8b8a4b3619bf1e6ac99b9
jcaperella29 <137432703+jcaperella29@users.noreply.github.com>
2025-04-09 13:33:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,7 @@ def download_summary():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
